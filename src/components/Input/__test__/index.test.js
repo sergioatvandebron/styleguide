af3a66d60ae3d7ee2d57faa3c5e17a4ea9602876
Francieli Viane <francielifrv@gmail.com>
2017-11-03 14:18:55 +0100

    Remove console.log from test

--- src/components/Input/__test__/index.test.js
+++ src/components/Input/__test__/index.test.js
@@ -7,7 +7,6 @@ describe('<Input/>', () => {
     const wrapper = render(<Input meta={{error:null}} type="text"/>)
 
     const input = wrapper.find('input');
-    console.log(input);
     expect(input.length).toEqual(1);
     expect(input[0].attribs.type).toBe("text");
     expect(input[0].attribs.class).toBe('Van-Text-Input');
