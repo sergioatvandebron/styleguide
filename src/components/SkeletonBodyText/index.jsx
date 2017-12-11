3de440854626b1e4e502f561582e9044e5c0a840
Bruno Queiros <brunoqueiros@vandebron.nl>
2017-12-11 14:20:04 +0100

    Create Skeleton components

---
+++ src/components/SkeletonBodyText/index.jsx
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './styles.scss';
+
+class SkeletonBodyText extends PureComponent {
+  render() {
+    const {
+      className,
+      lines,
+      ...props
+    } = this.props;
+    const textLines = [];
+
+    for (let i = 0; i < lines; i += 1) {
+      textLines.push(<div className="Van-SkeletonBodyText-line" key={i} />);
+    }
+
+    return (
+      <div
+        className={classnames('Van-SkeletonBodyText', className)}
+        {...props}
+      >
+        {textLines}
+      </div>
+    );
+  }
+}
+
+SkeletonBodyText.defaultProps = {
+  className: '',
+  lines: 3,
+};
+
+SkeletonBodyText.propTypes = {
+  className: PropTypes.string,
+  lines: PropTypes.number,
+};
+
+export default SkeletonBodyText;
---
+++ src/components/SkeletonDisplayText/index.jsx
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './styles.scss';
+
+class SkeletonDisplayText extends PureComponent {
+  render() {
+    const {
+      className,
+      size,
+      full,
+      ...props
+    } = this.props;
+    return (
+      <div
+        className={classnames('Van-SkeletonDisplayText', {
+          'Van-SkeletonDisplayText--small': size === 'small',
+          'Van-SkeletonDisplayText--medium': size === 'medium',
+          'Van-SkeletonDisplayText--large': size === 'large',
+          'Van-SkeletonDisplayText--extraLarge': size === 'extraLarge',
+          'Van-SkeletonDisplayText--full': full,
+        }, className)}
+        {...props}
+      />
+    );
+  }
+}
+
+SkeletonDisplayText.defaultProps = {
+  className: '',
+  size: 'medium',
+  full: false,
+};
+
+SkeletonDisplayText.propTypes = {
+  className: PropTypes.string,
+  size: PropTypes.string,
+  full: PropTypes.bool,
+};
+
+export default SkeletonDisplayText;
