65d3e7bc65ce80ce5f319fc7a80c5a1bf966c7dd
Bruno Queiros <brunoqueiros@vandebron.nl>
2018-01-09 17:42:14 +0100

    Update static path on catalog.css

--- scripts/generate-site.sh
+++ scripts/generate-site.sh
@@ -9,9 +9,10 @@ mkdir docs
 mv catalog/build/** docs/
 rm -r catalog/build
 sed -i -e "s/n.p=\"\/\"/n.p=\"\"/g" docs/static/manifest*
+sed -i -e "s/\/static\//static\//g" docs/static/*.css
 sed -i -e "s/\/static\//static\//g" docs/index.html
 rm docs/index.html-e
-rm docs/static/manifest*-e
+rm docs/static/*-e
 rm docs/static/*.map
 
 git add docs
