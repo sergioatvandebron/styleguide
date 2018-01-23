33af1c837bfa4e295208cb3235648eafdbb5fa21
Bruno Queiros <brunoqueiros@vandebron.nl>
2018-01-23 16:31:47 +0100

    ButtonGroup component

---
+++ src/components/ButtonGroup/index.jsx
@@ -0,0 +1,28 @@
+// import React, { PureComponent } from 'react';
+// import PropTypes from 'prop-types';
+// import classnames from 'classnames';
+// import './style.scss';
+
+// class ButtonGroup extends PureComponent {
+//   render() {
+//     const { className, image, children, ...props } = this.props;
+
+//     return (
+//       <div className={classnames('Van-ButtonGroup', className)} {...props}>
+//         {children}
+//       </div>
+//     );
+//   }
+// }
+
+// ButtonGroup.defaultProps = {
+//   className: '',
+//   children: undefined
+// };
+
+// ButtonGroup.propTypes = {
+//   className: PropTypes.string,
+//   children: PropTypes.element
+// };
+
+// export default ButtonGroup;
