5a07765ba01fb47dfdd44260bc8031cecca8f03c
Bruno Queiros <brunoofq@gmail.com>
2018-03-05 13:45:32 +0100

    Fix lint error

--- src/utils/is-svg.js
+++ src/utils/is-svg.js
@@ -1,4 +1,5 @@
 const htmlCommentRegex = require('html-comment-regex');
+
 const regex = /^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*\s*(?:\[?(?:\s*<![^>]*>\s*)*\]?)*[^>]*>\s*)?<svg[^>]*>[^]*<\/svg>\s*$/i;
 
 export default input => regex.test(input.toString().replace(htmlCommentRegex, ''));
