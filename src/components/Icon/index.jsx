26be94760c1a70438f534ff98e270f548864a042
Bruno Queiros <brunoofq@gmail.com>
2018-03-05 13:22:26 +0100

    Create a function to check if the string is svg

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -3,7 +3,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import isSVG from 'is-svg';
+import isSVG from '../../utils/is-svg';
 
 import './style.scss';
 
---
+++ src/utils/is-svg.js
@@ -0,0 +1,4 @@
+const htmlCommentRegex = require('html-comment-regex');
+const regex = /^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*\s*(?:\[?(?:\s*<![^>]*>\s*)*\]?)*[^>]*>\s*)?<svg[^>]*>[^]*<\/svg>\s*$/i;
+
+export default input => regex.test(input.toString().replace(htmlCommentRegex, ''));
