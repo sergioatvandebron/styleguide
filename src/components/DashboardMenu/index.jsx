5fb5efc6b3b8d5527e8c2467260af19988a5bdd7
Bruno Queiros <brunoofq@gmail.com>
2018-06-12 15:24:48 +0200

    Accept classname

--- src/components/DashboardMenu/index.jsx
+++ src/components/DashboardMenu/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import Item from './Item';
 import './styles.scss';
 
-const DashboardMenu = ({ children, ...props }) => (
-  <nav className="Van-DashboardMenu" {...props}>
+const DashboardMenu = ({ className, children, ...props }) => (
+  <nav className={classnames('Van-DashboardMenu', className)} {...props}>
     <ul className="Van-DashboardMenu-nav">{children}</ul>
   </nav>
 );
 
 DashboardMenu.defaultProps = {
-  children: null
+  children: null,
+  className: null
 };
 
 DashboardMenu.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  className: PropTypes.string
 };
 
 DashboardMenu.Item = Item;
