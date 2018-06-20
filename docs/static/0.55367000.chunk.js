ef990fab864da58ededdc49917a15229067cd1b9
Bruno Queiros <brunoofq@gmail.com>
2018-06-20 16:21:28 +0200

    Update website

--- docs/static/0.55367000.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],{954:function(e,t,n){var o=n(0),r=n(49),a=n(342).PageRenderer;a.__esModule&&(a=a.default);var c=r({displayName:"WrappedPageRenderer",getInitialState:function(){return{content:n(980)}},componentWillMount:function(){},render:function(){return o.createElement(a,Object.assign({},this.props,{content:this.state.content}))}});c.__catalog_loader__=!0,e.exports=c},980:function(e,t){e.exports="Intro\n"}});
-//# sourceMappingURL=0.55367000.chunk.js.map
