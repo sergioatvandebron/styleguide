c2d4e03aed03be7bf507c464449d136c262c95b0
Sérgio Miguel <sergiomiguel@vandebron.nl>
2018-08-17 10:32:43 +0200

    Add svg-inline-loader to docz' webpack config

--- packages/windmolen-web/doczrc.js
+++ packages/windmolen-web/doczrc.js
@@ -4,5 +4,24 @@ export default {
   src: './src',
   dest: '.docz/dist',
   base: '/web/',
-  plugins: [babel()]
+  plugins: [babel()],
+
+  modifyBundlerConfig: (config) => {
+    /** needed for Icon, loads svgs inline instead of return a url */
+    const idx = config.module.rules.findIndex(
+      r => r.test.toString() === '/\\.(svg)(\\?.*)?$/'
+    );
+
+    config.module.rules[idx] = {
+      test: /\.svg$/,
+      use: {
+        loader: 'svg-inline-loader',
+        options: {
+          removeTags: true
+        }
+      },
+    };
+
+    return config;
+  }
 };
