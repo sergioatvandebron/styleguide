1d40729c234b6e0fd5b27f1b5ead6276f1d7d4b1
Sérgio Miguel <sergiomiguel@vandebron.nl>
2018-08-17 17:06:56 +0200

    Add typing, withShadow prop and docz page to <Base /> component

--- packages/windmolen-web/src/components/Base.js
+++ packages/windmolen-web/src/components/Base.js
@@ -1,8 +1,14 @@
 // @flow
+import type { StatelessFunctionalComponent } from 'react';
 import styled from 'styled-components';
+import { largeUp } from '../utils';
 import { colors } from '../globals';
 
 export type Theme = 'lightBackground' | 'darkBackground';
+export type BaseProps = {
+  theme?: Theme,
+  withShadow?: boolean
+};
 
 const themeColor = (theme: ?Theme) => theme === 'darkBackground'
   ? colors.white
@@ -12,8 +18,9 @@ const themeBackgroundColor = (theme: ?Theme) => theme === 'darkBackground'
   ? colors.charcoalGray
   : colors.white;
 
-const Base = styled.div`
+const Base: StatelessFunctionalComponent<BaseProps> = styled.div`
   background-color: ${props => themeBackgroundColor(props.theme)};
+  box-shadow: ${props => props.withShadow ? '-22px 16px 22px 0 rgba(0, 0, 0, 0.18)' : 'unset'};
   color: ${props => themeColor(props.theme)};
   fill: ${props => themeColor(props.theme)};
   font-family: "Proxima Nova";
@@ -23,6 +30,8 @@ const Base = styled.div`
   font-weight: normal;
   letter-spacing: normal;
   line-height: 2;
+
+  ${props => props.withShadow ? largeUp('box-shadow: -40px 28px 40px 0 rgba(0, 0, 0, 0.18)') : ''}
 `;
 
 export default Base;
