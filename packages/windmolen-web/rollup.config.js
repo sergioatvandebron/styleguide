2201a775a2124e0547be48f408eca56cb4d76862
Sérgio Miguel <sergiomiguel@vandebron.nl>
2018-08-17 11:03:42 +0200

    Add rollup-svg-plugin

--- packages/windmolen-web/rollup.config.js
+++ packages/windmolen-web/rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import svg from 'rollup-plugin-svg';
 
 export default {
   input: 'src/index.js',
@@ -26,6 +27,8 @@ export default {
 
     terser({
       warnings: 'verbose'
-    })
+    }),
+
+    svg()
   ]
 }
