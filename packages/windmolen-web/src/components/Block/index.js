2605ce13c22eb68654e504518ce1b6387380fd2e
Sérgio Miguel <sergiomiguel@vandebron.nl>
2018-08-21 14:04:43 +0200

    Add outline block

--- packages/windmolen-web/src/components/Block/index.js
+++ packages/windmolen-web/src/components/Block/index.js
@@ -1,8 +1,10 @@
 import Base from '../Base';
+import { colors } from '../../globals';
 import { largeUp } from '../../utils';
 
 const Block = Base.withComponent('div').extend`
   padding: 30px;
+  border: ${props => props.outline ? `1px solid ${colors.white}` : '0'};
 
   ${largeUp('padding: 50px')}
 `;
