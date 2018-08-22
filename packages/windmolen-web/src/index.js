81c54a65b704772d634e2f4095eb7f70adbf4f0d
Sérgio Miguel <sergiomiguel@vandebron.nl>
2018-08-22 20:16:52 +0200

    Export Input

--- packages/windmolen-web/src/index.js
+++ packages/windmolen-web/src/index.js
@@ -7,3 +7,4 @@ export { default as Paragraph } from './components/Paragraph';
 export { default as Span } from './components/Span';
 export { default as Wrapper } from './components/Wrapper';
 export { H1, H2, H3, H4, H5 } from './components/Headings';
+export { default as Input } from './components/Input';
