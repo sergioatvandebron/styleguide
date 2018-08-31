f8d46e3862fdb009d33de1831389f1c221603ec6
Kim K <koomen@protonmail.com>
2018-08-31 12:21:44 +0200

    Added Image component

--- packages/windmolen-web/src/components/Image/index.js
+++ packages/windmolen-web/src/components/Image/index.js
@@ -1,7 +1,24 @@
+// @flow
+import React from 'react';
 import Base from '../Base';
+import { media } from 'styled-bootstrap-grid';
+import styled from 'styled-components';
 
-const Image = Base.withComponent('img').extend`
-  display: inline-block;
+export type Props = {
+  /** Will make the image not overflow its parent. */
+  responsive?: boolean,
+};
+
+const StyledImage = styled(Base.withComponent('img'))`
+  ${props => props.responsive ? 'max-width: 100%; height: auto;' : ''}
 `;
+
+const Image = (props: Props) => {
+  return <StyledImage {...props} />
+};
+
+Image.defaultProps = {
+  responsive: true,
+};
 
 export default Image;
