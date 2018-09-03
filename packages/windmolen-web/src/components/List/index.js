7821f3c1ccd656c0723a4affde2464ae15fd0844
Sérgio Miguel <sergiomiguel@vandebron.nl>
2018-09-03 13:43:40 +0200

    Small fixes to List

--- packages/windmolen-web/src/components/List/index.js
+++ packages/windmolen-web/src/components/List/index.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { type Node } from 'react';
 import styled from 'styled-components';
+import { media } from 'styled-bootstrap-grid';
 import Base from '../Base';
 import Span from '../Span';
 import Icon from '../Icon';
@@ -15,7 +16,7 @@ const Ul = Base.withComponent('ul').extend`
   list-style-type: none;
 `;
 
-const List = (({ children, ...props }): ListProps): Node => {
+const List = ({ children, ...props }: ListProps): Node => {
   const withIcon = React.Children.map(children, child => React.cloneElement(child, { inheritedIcon: props.icon }));
 
   return <Ul {...props}>{withIcon}</Ul>;
@@ -33,17 +34,26 @@ type ItemProps = {
 
 // Item
 const Li = Base.extend`
-  padding-left: 20px;
+  padding-left: 0;
   position: relative;
+
+  ${media.desktop`
+    padding-left: 20px;
+  `}
 `;
 
 const StyledIcon = styled(Icon)`
   position: absolute;
-  left: -0.5em;
-  top: 0.15em;
+  left: -0.17em;
+  top: 0.05em;
+
+  ${media.desktop`
+    left: -0.5em;
+    top: 0.15em;
+  `}
 `;
 
-const Item = ({ icon, inheritedIcon, children, ...props }): Node<ItemProps> => {
+const Item = ({ icon, inheritedIcon, children, ...props }: ItemProps) => {
   const usedIcon = icon || inheritedIcon;
 
   return (
